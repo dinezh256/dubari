440aa44bcb7b9bfbe49dcde0eaf801540dfd44fe
Dinesh Shaw <idineshshaw7@gmail.com>
2021-08-22 22:52:50 +0530

    meta tag

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import React from "react";
 import Landing from "../components/landing";
 import NavBar from "../components/navbar";
@@ -5,6 +6,10 @@ import NavBar from "../components/navbar";
 export default function Home() {
   return (
     <>
+    <Head>
+      <title>Dubari - Satindra Kumar Nath</title>
+      <meta title="description" content="Dubari - an assamese literature website" />
+    </Head>
       <NavBar />
       <Landing />
     </>
